aaf01e2645f4d20323c91ecd43524c4dfaaeb441
Usama Maqsood <hafizusamamaqsood@gmail.com>
2022-07-18 17:43:42 +0500

    make it better

--- portfolio/src/components/projects/projects.js
+++ portfolio/src/components/projects/projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./projects.css"
 import project1 from "../../images/Project1.png"
 import project2 from "../../images/Project2.png"
+import project3 from "../../images/Project3.png"
 
 const projects = () => {
   return (
@@ -18,6 +19,12 @@ const projects = () => {
           <a className='p-projectsItem' href='https://todo-mern-usama.herokuapp.com/' target="_blank">
            <img src={project2} className="p-1"/>
            <h2 className='p-projectTitle'>Todo List</h2>
+           <div className='p-overlay'></div>
+          </a>
+
+          <a className='p-projectsItem' href='https://calculator97.herokuapp.com//' target="_blank">
+           <img src={project3} className="p-1"/>
+           <h2 className='p-projectTitle'>Calculator</h2>
            <div className='p-overlay'></div>
           </a>
 
